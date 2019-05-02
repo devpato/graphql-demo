15c73c5ba24b9685220c86e905d112c078c76100
pevargasg <wearetamo@gmail.com>
2019-05-02 02:04:35 -0700

    sending get request with express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 import express from 'express';
+import { Server } from 'http';
+const SERVER = express();
+
+// SERVER.get('/graphql', (req, res) => {
+// 	res.send('<html><head><body><h1>Hellow World</h1></body></head></html>');
+// });
+
+SERVER.listen(4000, () => {
+	console.log('Listening to port 4000');
+});
