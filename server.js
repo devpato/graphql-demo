fff18807f1e2e400613423f65716b8cc2d30747e
pevargasg <wearetamo@gmail.com>
2019-05-04 01:28:43 -0700

    code clean up

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { makeExecutableSchema } from 'graphql-tools';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
-//Create fake data
-//addMockFunctionsToSchema({ schema });
-const server = new ApolloServer({ schema });
+const SERVER = new ApolloServer({ schema });
 
 const app = express();
-server.applyMiddleware({ app });
+SERVER.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${SERVER.graphqlPath}`));
