63bb764e8de96971c8ec0509d916ae2a00949354
pevargasg <wearetamo@gmail.com>
2019-05-04 01:20:12 -0700

    created const file

---
+++ constants.js
@@ -0,0 +1,22 @@
+const authors = [
+	{
+		id: 1,
+		name: 'Pato',
+		age: 27,
+		books: [ 'water rocks', 'usa life' ]
+	},
+	{
+		id: 2,
+		name: 'Derek',
+		age: 33,
+		books: [ 'mama mia', 'road trip' ]
+	},
+	{
+		id: 3,
+		name: 'Tony',
+		age: 30,
+		books: [ 'Tats' ]
+	}
+];
+
+export default authors;
--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-
-const authors = [
-	{
-		id: 1,
-		name: 'Pato',
-		age: 27,
-		books: [ 'water rocks', 'usa life' ]
-	},
-	{
-		id: 2,
-		name: 'Derek',
-		age: 33,
-		books: [ 'mama mia', 'road trip' ]
-	},
-	{
-		id: 3,
-		name: 'Tony',
-		age: 30,
-		books: [ 'Tats' ]
-	}
-];
+import authors from './constants';
 
 const typeDefs = gql`
 	type Author {
