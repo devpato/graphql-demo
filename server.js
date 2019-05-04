4039bb2a259d32b1910cee4f369ea837f0f920fd
pevargasg <wearetamo@gmail.com>
2019-05-04 01:45:56 -0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 import { makeExecutableSchema } from 'graphql-tools';
+import mongoose from 'mongoose';
 import typeDefs from './schema';
 import resolvers from './resolvers';
+
+mongoose.connect('mongodb+srv://pevargas:Policia9@@cluster0-us8bq.mongodb.net/test?retryWrites=true', {
+	useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+	console.log('Connection to DB was succesfull');
+});
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 const SERVER = new ApolloServer({ schema });
