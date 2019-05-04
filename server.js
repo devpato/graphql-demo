35e95bec0cc7e7429d74266322cd74c163d206c9
pevargasg <wearetamo@gmail.com>
2019-05-04 01:16:50 -0700

    basic structure of querying working

--- server.js
+++ server.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 
-// Construct a schema, using GraphQL schema language
+const authors = [
+	{
+		id: 1,
+		name: 'Pato',
+		age: 27,
+		books: [ 'water rocks', 'usa life' ]
+	},
+	{
+		id: 2,
+		name: 'Derek',
+		age: 33,
+		books: [ 'mama mia', 'road trip' ]
+	},
+	{
+		id: 3,
+		name: 'Tony',
+		age: 30,
+		books: [ 'Tats' ]
+	}
+];
+
 const typeDefs = gql`
+	type Author {
+		id: Int
+		age: Int
+		name: String
+		books: [String]
+	}
+
 	type Query {
-		hello: String
+		authors: [Author]
+		author(age: Int): Author
 	}
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
 	Query: {
-		hello: () => 'Hello world!'
+		authors: () => {
+			return authors;
+		},
+		author: (root, { age }) => {
+			return authors.find((a) => a.age === age);
+		}
 	}
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+//Create fake data
+//addMockFunctionsToSchema({ schema });
+
+const server = new ApolloServer({ schema });
 
 const app = express();
 server.applyMiddleware({ app });
